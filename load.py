c6c11f74eb4d26c54bc5992d00e25f1d8262aa47
qianyouwang <roywqy0101@gmail.com>
2023-03-10 15:32:41 -0800

    551hw3 update

--- load.py
+++ load.py
@@ -34,7 +34,7 @@ for inode in inodes:
     inode_permission = inode.xpath("./permission")[0].text if (inode.xpath("./permission")[0].text) else ""
     inode_data.append((inode_id, inode_type, inode_name, inode_replication, inode_mtime, inode_atime, inode_blocksize, inode_permission))
 inode_df = pd.DataFrame(inode_data, columns=["id", "type", "name", "replication", "mtime", "atime", "preferredBlockSize", "permission"])
-inode_df.to_sql("inode", con=conn, if_exists="append", index=False, engine)
+inode_df.to_sql("inode", con=conn, if_exists="append", index=False)
 
 # Extract directory information and store it in the database
 directories = tree.xpath("//directory")
@@ -48,7 +48,7 @@ for directory in directories:
         directory_data.append((parent, child_inumber))
         
 directory_df = pd.DataFrame(directory_data, columns=["parent", "child"])
-directory_df.to_sql("directory", con=conn, if_exists="append", index=False, engine)
+directory_df.to_sql("directory", con=conn, if_exists="append", index=False)
 
 # Close the database connection
 conn.close()
