888f9c0b3229f400c1b48552fcabe06b51f0f649
qianyouwang <roywqy0101@gmail.com>
2023-03-10 15:15:55 -0800

    551hw3 update

--- .ipynb_checkpoints/load-checkpoint.py
+++ .ipynb_checkpoints/load-checkpoint.py
@@ -21,16 +21,16 @@ conn = engine.connect()
 xml = etree.parse(image)
 
 # Extract inode information and store it in the database
-inodes = xml.xpath("//INode")
+inodes = xml.xpath("//inode")
 inode_data = []
 for inode in inodes:
     inode_id = int(inode.xpath("./id")[0].text)
     inode_type = inode.xpath("./type")[0].text
-    inode_name = inode.xpath("./name")[0].text
-    inode_replication = int(inode.xpath("./replication")[0].text)
+    inode_name = inode.xpath("./name")[0].text if (inode.xpath("./name")[0].text != "None") else ""
+    inode_replication = int(inode.xpath("./replication")[0].text) if inode.xpath("./replication") else 0
     inode_mtime = int(inode.xpath("./mtime")[0].text)
     inode_atime = int(inode.xpath("./atime")[0].text) if inode.xpath("./atime") else 0
-    inode_blocksize = int(inode.xpath("./preferredBlockSize")[0].text)
+    inode_blocksize = int(inode.xpath("./preferredBlockSize")[0].text) if inode.xpath("./preferredBlockSize") else 0
     inode_permission = inode.xpath("./permission")[0].text
     inode_data.append((inode_id, inode_type, inode_name, inode_replication, inode_mtime, inode_atime, inode_blocksize, inode_permission))
 inode_df = pd.DataFrame(inode_data, columns=["id", "type", "name", "replication", "mtime", "atime", "preferredBlockSize", "permission"])
@@ -42,11 +42,11 @@ directory_data = []
 
 for directory in directories:
     parent = int(directory.xpath("./parent")[0].text)
-    children = directory.xpath("./children/*")
+    children = directory.xpath("./child")
     for child in children:
-        child_inumber = int(child.xpath("./inode")[0].text)
+        child_inumber = int(child.text)
         directory_data.append((parent, child_inumber))
-
+        
 directory_df = pd.DataFrame(directory_data, columns=["parent", "child"])
 directory_df.to_sql("directory", con=conn, if_exists="append", index=False)
 
